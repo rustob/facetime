954333b14857adab9f6b1eb747a6f22a19a4fd49
linuxea <linuxea@foxmail.com>
2017-09-07 17:01:33 +0800

    init project

---
+++ src/main/java/com/tanner/controller/controller.java
@@ -0,0 +1,12 @@
+package com.tanner.controller;
+
+/**
+ * create by linuxea on 2017/9/7 16:06
+ **/
+public class controller {
+
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+
+}
