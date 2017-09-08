7f5b8a730fd855fe3a2701cc3bdc726a0c6a10cc
linuxea <linuxea@foxmail.com>
2017-09-08 16:13:28 +0800

    update configuration

--- src/main/java/com/tanner/service/TestServiceImpl.java
+++ src/main/java/com/tanner/service/TestServiceImpl.java
@@ -3,6 +3,9 @@ package com.tanner.service;
 import com.tanner.dao.testdao.UserMapper;
 import com.tanner.model.testmodel.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -24,11 +27,21 @@ public class TestServiceImpl implements TestService {
     private RedisTemplate<String, String> template;
 
     public void addUser(User user) {
-        User user1 = new User(1, "linuxea");
-        User user2 = new User(2, "tony");
+        User user1 = new User(99, "linuxea");
+        User user2 = new User(999, "tony");
         userMapper.addUser(user1);
-        int i = 10 / 0;
         userMapper.addUser(user2);
+
+        boolean result =
+                template.execute(new RedisCallback<Boolean>() {
+                    @Override
+                    public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+                        return connection.setNX("name".getBytes(), "linuxea".getBytes());
+                    }
+                });
+
+        System.out.println("###################################");
+        System.out.println(result);
     }
 
 }
