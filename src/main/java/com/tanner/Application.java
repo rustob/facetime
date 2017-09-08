cf4a3660505ce708c2bd73d69377f5c40947afbb
linuxea <linuxea@foxmail.com>
2017-09-08 12:44:37 +0800

    日志记录 logback 打印不出日志。暂时用 log4j 来代替

--- src/main/java/com/tanner/Application.java
+++ src/main/java/com/tanner/Application.java
@@ -2,6 +2,8 @@ package com.tanner;
 
 import com.tanner.com.tanner.service.TestService;
 import com.tanner.model.testmodel.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,10 +12,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  **/
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         TestService userService = (TestService) context.getBean("fuckService");
-        User user = new User(6, "linuxea");
+        User user = new User(23, "linuxea");
+        logger.info(user.toString());
         userService.addUser(user);
     }
 
