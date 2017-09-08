95b77fb27a84f9234ec265782f23b299cf71f99a
linuxea <linuxea@foxmail.com>
2017-09-08 18:42:37 +0800

    spring 与 redis 集成

--- src/main/java/com/tanner/Application.java
+++ src/main/java/com/tanner/Application.java
@@ -4,8 +4,13 @@ import com.tanner.model.testmodel.User;
 import com.tanner.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * create by linuxea on 2017/9/8 10:44
@@ -15,20 +20,10 @@ public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
 
-    public static void main(String[] args) {
-        Application application = new Application();
-        application.hello();
-//        application.jedis();
-    }
-
-
-    public void hello() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        TestService userService = (TestService) context.getBean("testServiceImpl");
-        User user = new User(693, "linuxea");
-        logger.info(user.toString());
-        userService.addUser(user);
-    }
+    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    // inject the actual template
+    @Autowired
+    private RedisTemplate<String, String> template;
 
 
     public void jedis() {
@@ -36,6 +31,33 @@ public class Application {
         TestService userService = (TestService) context.getBean("redisService");
         TestService testService = (TestService) context.getBean("redisService");
         testService.addUser(new User(98, "jedis test"));
+    }
+
+    @Resource(name = "redisTemplate")
+    private ListOperations<String, String> listOps;
+
+    public static void main(String[] args) {
+        Application application = new Application();
+//        application.hello();
+//        application.jedis();
+        application.addLink("name", "linuxea");
+    }
+
+    public void hello() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TestService userService = (TestService) context.getBean("testServiceImpl");
+        User user = new User(908, "linuxea");
+        logger.info(user.toString());
+        userService.addUser(user);
+    }
+
+    // inject the template as ListOperations
+    // can also inject as Value, Set, ZSet, and HashOperations
+    public void addLink(String userId, String value) {
+        template = (RedisTemplate<String, String>) context.getBean("redisTemplate");
+        listOps.leftPush(userId, value);
+        // or use template directly
+        template.boundListOps(userId).leftPush(value);
     }
 
 }
--- src/main/java/com/tanner/service/TestServiceImpl.java
+++ src/main/java/com/tanner/service/TestServiceImpl.java
@@ -3,6 +3,9 @@ package com.tanner.service;
 import com.tanner.dao.testdao.UserMapper;
 import com.tanner.model.testmodel.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +17,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(propagation = Propagation.NESTED)
 @Service
-public class TestServiceImpl implements TestService {
+public class TestServiceImpl extends RedisGeneratorDao<String, String> implements TestService {
 
     @Autowired
     private UserMapper userMapper;
 
     public void addUser(User user) {
+        boolean result =
+                super.redisTemplate.execute(new RedisCallback<Boolean>() {
+                    @Override
+                    public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+                        return connection.setNX("abc".getBytes(), "cde".getBytes());
+                    }
+                });
+        System.out.println(result);
         userMapper.addUser(user);
     }
 
