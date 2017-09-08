8617a18ded3fa817b9d82675d23be4901219624d
linuxea <linuxea@foxmail.com>
2017-09-08 14:38:53 +0800

    在具体的类使用事务注解而不是在接口的定义上

--- src/main/java/com/tanner/com/tanner/service/TestService.java
+++ src/main/java/com/tanner/com/tanner/service/TestService.java
@@ -1,13 +1,11 @@
 package com.tanner.com.tanner.service;
 
 import com.tanner.model.testmodel.User;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * create by linuxea on 2017/9/8 10:30
  **/
-@Transactional(propagation = Propagation.REQUIRED)
+
 public interface TestService {
 
     void addUser(User user);
--- src/main/java/com/tanner/com/tanner/service/TestServiceImpl.java
+++ src/main/java/com/tanner/com/tanner/service/TestServiceImpl.java
@@ -4,11 +4,14 @@ import com.tanner.dao.testdao.UserMapper;
 import com.tanner.model.testmodel.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * create by linuxea on 2017/9/8 10:31
  **/
 @Service("fuckService")
+@Transactional(propagation = Propagation.REQUIRED)
 public class TestServiceImpl implements TestService {
 
     @Autowired
